a96b6b62ce3cf92fa0fc7d31deefac95f498b55d
Kolbeinn Elí Pétursson <kolbeinn@ccpgames.com>
2025-01-14 16:12:39 +0000

    Update data loading path

--- src/GameRevenueChart.js
+++ src/GameRevenueChart.js
@@ -18,7 +18,7 @@ const GameRevenueChart = () => {
   const [showMedian, setShowMedian] = useState(false);
 
   useEffect(() => {
-    Papa.parse(`${process.env.PUBLIC_URL}/game_revenue_data.csv`, {
+    Papa.parse('https://kolbeinneli.github.io/revenue-chart/game_revenue_data.csv', {
       download: true,
       header: true,
       complete: (results) => {
